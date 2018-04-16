6fc3f62668938af5cd643ff24e438e2868ceaeb8
Nathan Jesudason <nathan.jesudason@gmail.com>
2018-04-16 12:41:47 -0700

    Add files via upload

---
+++ NathanPressureSensorMotor.ino
@@ -0,0 +1,20 @@
+
+int motorPin = 10; 
+int pressurePin = 0;
+int calibrationVal = 400;
+void setup()
+{
+ pinMode(motorPin, OUTPUT); 
+ Serial.begin(9600);
+}
+
+
+
+void loop()                   
+{
+  int val = analogRead(pressurePin);
+  Serial.println(val);
+  val = constrain(val, 0, calibrationVal);
+  val = map(val, 0, calibrationVal, 0, 255);
+  analogWrite(motorPin, val);
+}
